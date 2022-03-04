eafc18b0a56248a24c8ff4e6c2e3f3994adbbeaf
zoyaaskalda <zoyaaskalda@gmail.com>
2022-03-05 01:00:28 +0700

    Koneksi database dan datatables

---
+++ database/database.php
@@ -0,0 +1,31 @@
+<?php
+class Database
+{
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    public $conn;
+    function __construct()
+    {
+        $this->host = 'localhost';
+        $this->username = 'root';
+        $this->password = '';
+        $this->db_name = 'penggajian_7s';
+    }
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host .
+            ";dbname=" . $this->db_name, 
+            $this->username, 
+            $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, 
+            PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
