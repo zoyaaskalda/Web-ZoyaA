41f41790562456bcd5be337d8a3359e624537725
zoyaaskalda <zoyaaskalda@gmail.com>
2022-03-05 23:30:41 +0700

    Membuat Perintah Update Data

--- pages/admin/lokasiupdate.php
+++ pages/admin/lokasiupdate.php
@@ -4,14 +4,47 @@ if (isset($_GET['id'])) {
     $database = new Database();
     $db = $database->getConnection();
 
-    $id = $ $_GET['id'];
+    $id = $_GET['id'];
     $findSql = "SELECT * FROM lokasi WHERE id = ?";
-    $stmt =$db=>prepare($findSql);
+    $stmt = $db->prepare($findSql);
     $stmt->bindParam(1, $_GET['id']);
     $stmt->execute();
     $row = $stmt->fetch();
     if(isset($row['id'])) {
+        if (isset($_POST['button_update'])) {
+
+            $database = new Database();
+            $db = $database->getConnection();
+
+            $validateSql = "SELECT * FROM lokasi WHERE nama_lokasi = ? AND id !=?";
+            $stmt =$db->prepare($validateSQL);
+            $stmt->bindParam(1, $_POST['nama_lokasi']);
+            $stmt->bindParam(2, $_POST['id']);
+            $stmt->execute();
+            if($stmt->rowCount() > 0){
 ?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+            Nama Lokasi sama sudah ada 
+        </div>
+    <?php
+    } else {
+        $insertSQL = "UPDATE lokasi SET nama_lokasi = ? WHERE id =?";
+        $stmt = $db->prepare($updateSQL);
+        $stmt->bindParam(1, $_POST['nama_lokasi']);
+        $stmt->bindParam(2, $_POST['id']);
+        if ($stmt->execute()) {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Berhasil Ubah Data";
+        } else {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "Gagal ubah Data";
+        }
+        echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+     }
+    }
+    ?>
 <section class="content-header">
   <div class="container-fluid">
     <div class="row mb2">
