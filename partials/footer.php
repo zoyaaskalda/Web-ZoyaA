020e3c4c362bd0e361723ece6dd0c06c26e986cb
zoyaaskalda <zoyaaskalda@gmail.com>
2022-03-05 21:35:00 +0700

    Menampilkan data lokasi

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-inline">
-      A,<b>Version</b> 0.1.0
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 0.1.0
     </div>
     <strong>Copyright &copy; 2022 <a href="#">Zoya Askalda</a>.</strong> All rights reserved.
-  </footer>
+</footer>
