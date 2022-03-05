2f2e1565bbb5d28d24b5547bd48d337bf62eb59f
zoyaaskalda <zoyaaskalda@gmail.com>
2022-03-05 21:27:12 +0700

    Menampilkan halaman read lokasi

--- routes.php
+++ routes.php
@@ -4,10 +4,13 @@ if (isset($_GET['page'])) {
     switch ($page) {
         case '':
         case 'home':
-            file_exists('page/home.php') ? include 'pages/home.php' : include "pages/404.php";
+            file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
+            break;
+        case 'lokasiread':
+            file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include "pages/404.php";
             break;
         default:
-            include "page/404.php";
+            include "pages/404.php";
     }
 } else {
     include "pages/home.php";
